e14315a26d173463d4093be08aa9c0d44e2ce129
Stephen Morrison <srmorrisonjit@gmail.com>
2016-02-18 22:59:16 -0500

    initial skeleton

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const handlebars = require('express-handlebars');
+const mysql = require('mysql');
+
+const baseRoutes = require('./routes/base')
+
+const app = express();
+
+// use handlebars for templates
+app.engine('.html', handlebars({defaultLayout: 'main', extname: '.html'}));
+app.set('view engine', '.html');
+
+app.use((req, res, next) => {
+	// dictionary of request global variables
+	req.App = {};
+	next();
+});
+
+// setup MySQL
+app.use((req, res, next) => {
+	const connection = mysql.createConnection({
+	  host     : process.env.CLASS_DB_HOST,
+	  user     : process.env.CLASS_DB_USER,
+	  password : process.env.CLASS_DB_PASSWORD,
+	  database : process.env.CLASS_DB_DATABASE
+	});
+
+	connection.connect()
+
+	req.App.mysql = connection;
+
+	// end connection once the res
+	res.on('finish', ((req) => () => {
+		return req.App.mysql.end();
+	})(req));
+
+	next();
+});
+
+// routes
+app.use('/', baseRoutes);
+
+// start server
+const port = process.env.CLASS_PORT || 3000;
+
+app.listen(port, () => {
+	console.log('Server running at http://localhost:' + port);
+});
