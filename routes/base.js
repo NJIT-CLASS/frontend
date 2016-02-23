6e92443b4b9f16ee8d43d799cdbe13b96cee61da
ala25 <ala25@njit.edu>
2016-02-22 22:42:02 -0500

    adding i18n for transaltion, its working for en for now we will need to add more langauges in the future as a cookie

--- routes/base.js
+++ routes/base.js
@@ -1,10 +1,12 @@
 const express = require('express');
-
+//custom translation script
+var i18n = require('i18n');
 const router = express.Router();
 
 router.get('/', (req, res) => {
+	//console.log(res.__('Hello i18n'));
     res.render('home', {
-        title: 'CLASS Home'
+        title: res.__('Hello i18n')
     });
 });
 
--- server.js
+++ server.js
@@ -4,11 +4,21 @@ const handlebars = require('express-handlebars');
 const baseRoutes = require('./routes/base');
 
 const app = express();
+const i18n = require('i18n');
+
+//transalting power is present here
+i18n.configure({
+  locales: ['en', 'es'],
+  cookie: 'yourcookiename',
+  directory: __dirname+'/locales'
+});
+app.use(i18n.init);
 
 // use handlebars for templates
 app.engine('.html', handlebars({defaultLayout: 'main', extname: '.html'}));
 app.set('view engine', '.html');
 app.use(express.static(__dirname + '/css'));
+
 
 app.use((req, res, next) => {
     // dictionary of request global variables
