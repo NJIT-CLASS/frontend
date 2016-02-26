f667c32fbf23e89f8aa243879d2cc524a1512f27
Stephen Morrison <srmorrisonjit@gmail.com>
2016-02-26 16:00:55 -0500

    post not get

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ app.use((req, res, next) => {
             }
 
             const options = {
-                method: 'GET',
+                method: 'POST',
                 uri: `http://192.241.189.38:8080/api${endpoint}`,
                 json: true,
                 body: body
