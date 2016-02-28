47d33bb5116d849f2b33fee0b6588d79f6ab06cd
Stephen Morrison <srmorrisonjit@gmail.com>
2016-02-27 21:47:20 -0500

    get initial react page set up

---
+++ react/create-course/container.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Container extends React.Component {
+    render() {
+        return <h3>Class Name</h3>;
+    }
+}
+
+export default Container;
---
+++ react/create-course/main.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Container from './container';
+
+ReactDOM.render(<Container />, document.getElementById('create-course-container'));
