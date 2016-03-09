46a492053cf8cebaad7512d166ba5d560c48bd94
ala25 <ala25@njit.edu>
2016-03-08 22:01:55 -0500

    -n

--- react/create-assignment/page-header.js
+++ react/create-assignment/page-header.js
@@ -3,9 +3,21 @@ import React from 'react';
 class PageHeader extends React.Component {
     render() {
         return (
+        	<div>
             <div className="page-header">
                 <h2>Create Assignment</h2>
             </div>
+            	<div className="tab">
+				  <ul>
+				    <li><a href="#">Option 1</a></li>
+				    <li><a href="#">Option 2</a></li>
+				    <li><a href="#">Option 3</a></li>
+				    <li><a href="#" className="lastO" >Option 4</a></li>
+				  </ul>
+				  </div>
+            </div>
+
+
         );
     }
 }
