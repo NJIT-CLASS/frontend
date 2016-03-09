27972bd992c14aea7920963dcd64aa7ba5680605
ala25 <ala25@njit.edu>
2016-03-09 18:12:47 -0500

    adding a card for workflow detials view

--- react/create-assignment/workflow-detail-container.js
+++ react/create-assignment/workflow-detail-container.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import TaskForm from './task-form';
 
 class WorkflowDetailContainer extends React.Component {
     render() {
         return (
             <div className="assignment-workflow-details">
-                
+                <TaskForm/>
             </div>
         );
     }
