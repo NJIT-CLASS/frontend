2453313ed3237a1647a25920d77e0f50fe1ae9fa
Stephen Morrison <srmorrisonjit@gmail.com>
2016-05-03 11:40:03 -0400

    add ability to make instructors

--- server/routes/route-handlers/administrator.js
+++ server/routes/route-handlers/administrator.js
@@ -1,7 +1,30 @@
+const async = require('async');
+
+function getInstructorEmails(req, instructor, cb) {
+    req.App.api.get(`/generalUser/${instructor.UserID}`, (err, statusCode, body) => {
+        cb(null, {
+            email: body.User[0].Email,
+            userId: instructor.UserID,
+            admin: instructor.Admin
+        });
+    });
+}
+
 exports.get = (req, res) => {
     req.App.api.get('/instructor/all', (err, statusCode, body) => {
-        res.render('admin', {
-            instructors: body.Instructors
+        async.map(body.Instructors, getInstructorEmails.bind(null, req), (err, results) => {
+            res.render('admin', {
+                instructors: results
+            });
         });
     });
 };
+
+exports.post = (req, res) => {
+    if (!req.body.email) {
+        return res.redirect(req.route.path);
+    }
+    req.App.api.put('/instructor/new', {email: req.body.email}, (err, statusCode, body) => {
+        return res.redirect(req.route.path);
+    });
+}
