c9bc7444eba3d22364c6166afbf6960c34890c2e
Alan Romano <alan.rom25.95@gmail.com>
2017-01-30 16:39:46 -0500

    Add Section onSubmit added.

--- react/add-section/add-section-container.js
+++ react/add-section/add-section-container.js
@@ -9,13 +9,15 @@ class AddSectionContainer extends React.Component {
       super(props);
       this.state = {
         courses: null,
+        description:'',
         selected_course: null,
         semesters: [],
         semesterId: '',
         semester_name: '',
         section_identifier:'',
         start_date: '',
-        end_date: ''
+        end_date: '',
+        submitted: null
       }
   }
 
@@ -58,7 +60,33 @@ class AddSectionContainer extends React.Component {
 
   onSubmit(){
     //need to make API to handle this
+    if(!this.state.semesterId || !this.state.selected_course){
+      this.setState({submitted: false});
+      return;
+    }
 
+    const options = {
+        method: 'POST',
+        uri: this.props.apiUrl + '/api/course/createsection',
+        body: {
+            semesterid: this.state.semesterId,
+            courseid: this.state.selected_course,
+            name:this.state.section_identifier,
+            description: this.state.description,
+            organizationid: 1
+        },
+        json: true
+    };
+
+    request(options, (err, res, body) => {
+      if(err || res.statusCode == 401 ){
+        console.log("Error submitting!");
+        this.setState({submitted: false});
+        return;
+      }
+      this.setState({submitted: true});
+      console.log("Section created", body.response);
+    });
   }
 
   onChangeCourse(course){
@@ -88,6 +116,23 @@ class AddSectionContainer extends React.Component {
   }
 
   render() {
+      let message = null;
+      if(this.state.submitted == true){
+        message = (<span onClick={() => {
+            this.setState({submitted: null})
+        }} className="small-info-message">
+        <span className="success-message">
+          Successfully created section!
+        </span>
+      </span>);
+      }
+      else if(this.state.submitted == false) {
+        message = (<span onClick={() => {
+            this.setState({submitted: null})
+        }} className="small-info-message">
+        <div className="error-message">Submit Error!</div>
+        </span>);
+      }
 
       let semestersList = clone(this.state.semesters);      // using lodash here
       //semestersList.push({ value: "create", label: 'Create new semester...' });
@@ -145,18 +190,22 @@ class AddSectionContainer extends React.Component {
 
       //alert(this.state.courses);
       return(
-        <div className="section add-section-details">
-            <h2 className="title">Select Course</h2>
-            <form className="section-content">
-            <label>Course</label>
-            <div>
-              <Select options={this.state.courses} value={this.state.selected_course} onChange={this.onChangeCourse.bind(this)} resetValue={''} clearable={true} searchable={false}/>
-              { createSection }
-              { createSemester }
-              <button type="button" onClick={this.onSubmit.bind(this)}>Submit</button>
-            </div>
-            </form>
+        <div>
+          {message}
+          <div className="section add-section-details">
+              <h2 className="title">Select Course</h2>
+              <form className="section-content">
+              <label>Course</label>
+              <div>
+                <Select options={this.state.courses} value={this.state.selected_course} onChange={this.onChangeCourse.bind(this)} resetValue={''} clearable={true} searchable={false}/>
+                { createSection }
+                { createSemester }
+                <button type="button" onClick={this.onSubmit.bind(this)}>Submit</button>
+              </div>
+              </form>
+          </div>
         </div>
+
 
       );
   }
