e0f5da8f4e70f8326b97b440d668269e4c3ec7e8
Alan Romano <alan.rom25.95@gmail.com>
2017-08-03 19:55:02 -0400

    Change default backend port to match backend repo

--- server/utils/react_constants.js
+++ server/utils/react_constants.js
@@ -1,6 +1,6 @@
 const path = require('path');
 
-exports.API_URL = process.env.API_URL || 'http://localhost:3000' ;
+exports.API_URL = process.env.API_URL || 'http://localhost:4000' ;
 
 //Language Options
 exports.LANGUAGES = [{
