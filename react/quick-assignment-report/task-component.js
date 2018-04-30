412b5c2e931d393668d51f929a08d6c81de63cb3
Jerrod Ransom <jtr27@njit.edu>
2018-04-30 14:22:17 -0400

    Fix issue with filters not working on all of the workflow activities

--- react/quick-assignment-report/task-component.js
+++ react/quick-assignment-report/task-component.js
@@ -2,27 +2,29 @@ import React, { Component } from 'react';
 import TaskInstanceComponent from './task-instance-component';
 
 const TaskComponent = ({currentUserID, hasInstructorPrivilege, TaskActivity, TA_ID, WI_ID, WA_ID, Filters, Strings, onReplaceUserInTaskButtonClick, onMoreInformationButtonClick}) => {
-    let showTaskActivity = true;
-    if (Filters.Type.length > 0) {
-        showTaskActivity = Filters.Type.includes(parseInt(TA_ID, 10));
-    }
-    let taskInstancesArray = TaskActivity.map((taskInstance, index)=> {
-        return <TaskInstanceComponent TaskInstance={taskInstance}
-                                      key={`${WA_ID}-${WI_ID}-${TA_ID}-${index}`}
-                                      Filters={Filters}
-                                      Strings={Strings}
-                                      onReplaceUserInTaskButtonClick={onReplaceUserInTaskButtonClick}
-                                      onMoreInformationButtonClick={onMoreInformationButtonClick}
-                                      hasInstructorPrivilege={hasInstructorPrivilege}
-                                      currentUserID={currentUserID}
-                                  />;
-    });
+    let taskInstancesArray = TaskActivity
+        .filter(taskInstance => Filters.Type.length === 0 || Filters.Type.includes(taskInstance.TaskActivity.Type))
+        .map((taskInstance, index) => {
+            return (
+                <TaskInstanceComponent
+                    TaskInstance={taskInstance}
+                    key={`${WA_ID}-${WI_ID}-${TA_ID}-${index}`}
+                    Filters={Filters}
+                    Strings={Strings}
+                    onReplaceUserInTaskButtonClick={onReplaceUserInTaskButtonClick}
+                    onMoreInformationButtonClick={onMoreInformationButtonClick}
+                    hasInstructorPrivilege={hasInstructorPrivilege}
+                    currentUserID={currentUserID}
+                />
+            );
+        });
 
-    if(showTaskActivity){
-        return <div className='task-activity-block'>{taskInstancesArray}</div>;
-    }
-    else{
-        return <div className={'task-activity-block empty-block-placeholder'}></div>;
+    if (taskInstancesArray.length > 0) {
+        return <div className="task-activity-block">{taskInstancesArray}</div>;
+    } else {
+        return (
+            <div className={'task-activity-block empty-block-placeholder'} />
+        );
     }
 };
 
