3e2515cf092579b00fcc48dc350f830cba386cf9
Alan Romano <alan.rom25.95@gmail.com>
2018-05-04 06:13:14 -0400

    Fix issue with task template

--- server/routes/route-handlers/task-template.js
+++ server/routes/route-handlers/task-template.js
@@ -5,9 +5,14 @@ exports.get = (req, res) => {
     if(req.App.user === undefined){
         return res.redirect(`/?url=${encodeURIComponent(req.originalUrl)}`);
     }
+
+    var userIDToUse = req.App.user.userId;
+    if(req.session.masqueraderId != null && req.session.masqueraderId != undefined){
+        userIDToUse = req.session.masqueraderId;
+    }
     res.render('task-template', {
         scripts: ['/static/react_apps.js'],
-        userId: req.session.masqueraderId || req.App.user.userId,
+        userId: userIDToUse ,
         taskId: req.params.taskId,
         courseId: req.query.courseId,
         sectionId: req.query.sectionId,
