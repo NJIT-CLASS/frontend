6aee58e5feef4b32d2e74d3f1e74a19757bbe9cb
Alan Romano <alan.rom25.95@gmail.com>
2018-05-08 19:34:14 -0400

    Fix task status table

--- server/routes/route-handlers/task-status-table.js
+++ server/routes/route-handlers/task-status-table.js
@@ -4,7 +4,6 @@ exports.get = (req, res) => {
     if(req.App.user === undefined){
         return res.redirect(`/?url=${encodeURIComponent(req.originalUrl)}`);
     }
-
     res.render('./task-status-table', {
         scripts: ['/static/react_apps.js'],
         userId: req.App.user.userId,
