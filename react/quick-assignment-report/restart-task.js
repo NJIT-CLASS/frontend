e7de23efcb0780d5ac9f0df5a9dd4f5edaa7e839
Jerrod Ransom <jtr27@njit.edu>
2018-05-14 06:14:13 -0400

    Make a skeleton for the 'restart task' confirmation popup.'

--- react/quick-assignment-report/restart-task.js
+++ react/quick-assignment-report/restart-task.js
@@ -2,48 +2,54 @@ import React, { Component } from 'react';
 import Modal from '../shared/modal';
 import apiCall from '../shared/apiCall';
 
-class CancelTask extends Component {
+// This component renders a popup message asking for confirmation when a user tries to restart a task.
+// On confirmation, it should perform the restart.
+// TODO: This still must be implemented.
+class RestartTask extends Component {
     constructor(props) {
         super(props);
     }
 
-    cancelTask() {
+    restartTask() {
+        /* TODO: Implement this when the backend API is ready.
+
         const taskInstanceID = this.props.taskInstance.TaskInstanceID;
 
         const postBody = {
-            ti_id: taskInstanceID
+
         };
 
-        // See 'Cancel a single task' section of the 'Pool and Reallocation APIs' document
-        // for information about this API call
-        // (https://drive.google.com/open?id=1IID3sbmgdTUW2X5E7Buve18UnDR3cM-k)
-        const url = '/task/cancel';
+        const url = '';
+
         apiCall.postAsync(url, postBody)
             .then(() => {
-                showMessage('Task successfully cancelled');
-                this.props.onCancelTask();
+                showMessage('Task successfully restarted');
+                this.props.onRestartTask();
             });
+
+        */
+
         this.props.onClose();
     }
 
     render() {
-        const title = 'Are you sure you want to cancel this task?';
-        const okLabel = 'Cancel this task';
-        const cancelLabel = 'Don\'t cancel this task';
+        const title = 'Are you sure you want to restart this task?';
+        const okLabel = 'Restart this task';
+        const cancelLabel = 'Don\'t restart this task';
 
         const taskID = this.props.taskInstance.TaskInstanceID;
+        const taskName = this.props.taskInstance.TaskActivity.DisplayName;
 
         const message = 
             <div>
-                <p>The following task will be cancelled: </p>
+                {/* TODO: remove this message when implemented */}
+                <p> Note: This feature is not yet implemented. </p>
+                <p>The following task will be restarted: </p>
                 <ul>
                     <li>
-                        {taskID}
+                        {`${taskName} (ID: ${taskID})`}
                     </li>
                 </ul>
-                <p style={{fontSize: 'smaller'}}>
-                    Note: Cancelling this task will stop all or part of the problem thread from continuing.
-                </p>
             </div>;
 
         return (
@@ -63,7 +69,7 @@ class CancelTask extends Component {
                     <button
                         className="button"
                         id="confirm-button"
-                        onClick={() => this.cancelTask()}
+                        onClick={() => this.restartTask()}
                     >
                         {okLabel}
                     </button>
@@ -73,4 +79,4 @@ class CancelTask extends Component {
     }
 }
 
-export default CancelTask;
+export default RestartTask;
