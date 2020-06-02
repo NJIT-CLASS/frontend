2d8363fce14e71b2c8e137347bc604690dd939c9
Zoraiz Naeem <zoraiznaeem9@gmail.com>
2020-06-01 21:29:12 -0400

    removed the print statement in the tooltip.js

--- react/shared/tooltip.js
+++ react/shared/tooltip.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactTooltip from 'react-tooltip';
 
+// Multiline does not work maybe want to fix it!
 const Tooltip = (props) => {
   const {Text, ID, style, multiline} = props;
   return (
     <div style={{ ...style, display: 'inline-block' }}>
-      {console.log(Text)}
       <i className="fa fa-info-circle tooltip-icon"  aria-hidden="true" data-tip={Text} data-for={ID}></i>
         <ReactTooltip html={true} className="react-tooltip-custom" id={ID} effect='solid' multiline={multiline} {...props}>
 
