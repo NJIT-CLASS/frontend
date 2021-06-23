b5031d653d00a7208bb6a16c3bef7f4018db4e51
smehta23 <47726705+smehta23@users.noreply.github.com>
2021-06-22 20:00:20 -0400

    Updated with new backend data and complies with Grade Report V30

--- react/grade-report/AssignmentExtraCreditTaskGradeFieldsReport.js
+++ react/grade-report/AssignmentExtraCreditTaskGradeFieldsReport.js
@@ -2,6 +2,7 @@ import React from 'react';
 import apiCall from '../shared/apiCall';
 import TableComponent from '../shared/tableComponent';
 import * as ECG from './ExtraCreditGradeCalculation';
+import MultiTaskGradeTable from './MultiTaskGradeTable';
 
 
 class AssignmentExtraCreditTaskGradeFieldsReport extends React.Component {
@@ -16,84 +17,26 @@ class AssignmentExtraCreditTaskGradeFieldsReport extends React.Component {
 
     render(){
         //return (<div>The full grade report page is under development and will be ready in late Spring 2019.   You can see the grades for individual tasks from the "All Assignments Status" page.  Look for your submission, and then you can see the grades further along its problem thread.</div>);
-        let {strings, ECTGFRData} = this.props;
-        let TableECTGFRData = [];
+        let {strings, numOfTaskGrades, taskID, ECTGFRData} = this.props;
 
         console.log("ECTGFRData");
-            console.log(ECTGFRData);
+        console.log(ECTGFRData);
 
-        for (var taskGradeFieldNum in ECTGFRData){
-            let taskGradeField = ECTGFRData[taskGradeFieldNum];
-            for (var taskFieldNum in taskGradeField){
-                let taskField = taskGradeField[taskFieldNum];
-                
-                TableECTGFRData.push({
-                    Field: taskField.name == null ? "Unnamed" : taskField.name, 
-                    Type: taskField.type, 
-                    Value: taskField.value, 
-                    Max: ECG.getMax(taskField.type, taskField.max),
-                    ConvertedNumericValue: ECG.getConvNumGrade(taskField.value, taskField.type, ECG.getMax(taskField.type, taskField.max)),
-                    WeightWTask: isNaN(taskField.weight) ? "-" : taskField.weight,
-                    ScaledGrade: ECG.getConvNumGradeScaled(taskField.value, taskField.type, 
-                                                        ECG.getMax(taskField.type, taskField.max), taskField.weight)
-                });
-            }
-
-        }
-
+        let singleTaskGrade = (numOfTaskGrades == 1);
+        let TableECTGFRDataFrame = ECG.getTaskFieldReport(ECTGFRData, numOfTaskGrades);
             
 
 
 
         return (
-            <div className="section card-2 sectionTable">
-                <h2 className="title">{strings.ECTGFRHeader}</h2>
-                <div className="section-content">
-                    <div className="col-xs-6">
-                    <TableComponent
-                            data={TableECTGFRData}
-                            columns={[
-                                {
-                                    Header: strings.Field,
-                                    accessor: 'Field',
-                                    resizable:true      
-                                },
-                                {
-                                    Header: strings.Type,
-                                    accessor: 'Type',
-                                    resizable:true
-                                },
-                                {         
-                                    Header: strings.Value,
-                                    accessor: 'Value',
-                                    resizable:true                              
-                                },
-                                {
-                                    Header: strings.Max,
-                                    resizable:true,
-                                    accessor: 'Max'
-                                },
-                                {
-                                    Header: strings.ConvertedNumericValue,
-                                    accessor: 'ConvertedNumericValue',
-                                    resizable: true
-                                },
-                                {
-                                    Header: strings.WeightWTask,
-                                    resizable:true,
-                                    accessor: 'WeightWTask'
-                                },
-                                {
-                                    Header: strings.ScaledGradeTask,
-                                    resizable:true,
-                                    accessor: 'ScaledGrade'
-                                }
-                            ]}
-                            noDataText={strings.TaskGradeNoData}
-                        />
-                    </div>
-                </div>
-            </div>
+            <MultiTaskGradeTable
+                multiTaskGradeFieldsData={TableECTGFRDataFrame}
+                singleTaskGrade={singleTaskGrade}
+                numOfTaskGrades={numOfTaskGrades}
+                extraCrediTaskGrade={true}
+                taskID={taskID}
+                strings={strings}>
+            </MultiTaskGradeTable>
         );
     
     }
--- react/grade-report/TaskGradeFieldsReport.js
+++ react/grade-report/TaskGradeFieldsReport.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import apiCall from '../shared/apiCall';
 import TableComponent from '../shared/tableComponent';
+import MultiTaskGradeTable from './MultiTaskGradeTable';
 
 
 class TaskGradeFieldsReport extends React.Component {
@@ -16,96 +17,183 @@ class TaskGradeFieldsReport extends React.Component {
 
     render(){
         //return (<div>The full grade report page is under development and will be ready in late Spring 2019.   You can see the grades for individual tasks from the "All Assignments Status" page.  Look for your submission, and then you can see the grades further along its problem thread.</div>);
-        let {strings, TGFRGradeData} = this.props;
+        let {strings, numOfTaskGrades, taskID, TGFRGradeData} = this.props;
         let {loaded} = this.state;
         console.log("TGFRGradeData");
         console.log(TGFRGradeData);
 
-        var finalGradeFields = null;
+
+
+        var TableTGFRGradeDataFrame = [];
         var TableTGFRGradeData = [];
+        // let tableHeaders = [];
+
+        var singleTaskGrade = (numOfTaskGrades == 1);
+
+        var taskNumber = 1;
         for(var key in TGFRGradeData){
-            finalGradeFields = TGFRGradeData[key];
+            var finalGradeFields = TGFRGradeData[key];
+            // if (multipleTaskGrades) tableHeaders.push(taskNumber);
 
             for(var taskGradeID in finalGradeFields){
-                var taskGrade = null;
+                var taskGrade = finalGradeFields[taskGradeID]; 
+
+                var value = null;
+                var maxValue = null;
                 var convNumGrade = null;
-                var scaledGrade = null;
-                taskGrade = finalGradeFields[taskGradeID];
-                if (taskGrade.type === "Label")
-                    convNumGrade = 100;
-                else if (taskGrade.type === "Pass/Fail")
-                    convNumGrade = taskGrade.value == "pass" ? 100 : 0;
-                else 
-                    convNumGrade = ((taskGrade.value/taskGrade.max) * 100).toFixed(2); 
+                
+                if (taskGrade.type === "Pass/Fail" || taskGrade.type === "Label" || taskGrade.type === "Rating" || taskGrade.type === "Numeric"){
+                    if (taskGrade.type === "Label"){
+                        value = taskGrade.value;
+                        maxValue = taskGrade.labelMaxValue + " (" + taskGrade.max + ")";  
+                        convNumGrade = taskGrade.convertedNumericValue + " (" + taskGrade.labelPosition + ")";
+
+                    } else if (taskGrade.type === "Pass/Fail"){
+                        value = taskGrade.value;
+                        maxValue = "pass";
+                        convNumGrade = taskGrade.convertedNumericValue;
+                    } else {
+                        value = taskGrade.value;
+                        maxValue = taskGrade.max;
+                        convNumGrade = taskGrade.convertedNumericValue;
+                    }
+                    
+                
+                
+                    TableTGFRGradeData.push({
+                        Field:taskGrade.name != null ? taskGrade.name: "Unnamed",
+                        Type:taskGrade.type,
+                        Value: value,
+                        ConvertedNumericValue: convNumGrade,
+                        Max: maxValue,
+                        WeightWTask: taskGrade.weight,
+                        ScaledGrade: taskGrade.scaledGrade
+                    });
+                }
                 
 
-                if (!isNaN(convNumGrade * (taskGrade.weight/100))){
-                    scaledGrade = (convNumGrade * (taskGrade.weight/100)).toFixed(2)
-                }
-
-                TableTGFRGradeData.push({
-                    Field:taskGrade.name != null ? taskGrade.name: "Unnamed",
-                    Type:taskGrade.type,
-                    Value: isNaN(taskGrade.value) ? "-" : taskGrade.value,
-                    ConvertedNumericValue: isNaN(convNumGrade) ? "-" : convNumGrade,
-                    Max: taskGrade.max,
-                    WeightWTask:taskGrade.weight,
-                    ScaledGrade: isNaN(scaledGrade) ? "-" : scaledGrade
-                });
             }
+            TableTGFRGradeDataFrame.push(TableTGFRGradeData);
+            TableTGFRGradeData = [];
         }
         
+        // if (numOfTaskGrades == 0){
+        //     return (
+        //         <div className="section card-2 sectionTable">
+        //         <h2 className="title">{strings.TGFRHeader + ": " + taskID}</h2>
+        //         <div className="section-content">
+        //             <div className="col-xs-6">
+        //                 <TableComponent
+        //                     data={taskGradeData}
+        //                     columns={[
+        //                         {
+        //                             Header: strings.Field,
+        //                             accessor: 'Field',
+        //                             resizable:true      
+        //                         },
+        //                         {
+        //                             Header: strings.Type,
+        //                             accessor: 'Type',
+        //                             resizable:true
+        //                         },
+        //                         {         
+        //                             Header: strings.Value,
+        //                             accessor: 'Value',
+        //                             resizable:true                              
+        //                         },
+        //                         {
+        //                             Header: strings.Max,
+        //                             resizable:true,
+        //                             accessor: 'Max'
+        //                         },
+        //                         {
+        //                             Header: strings.ConvertedNumericValue,
+        //                             accessor: 'ConvertedNumericValue',
+        //                             resizable: true
+        //                         },
+        //                         {
+        //                             Header: strings.WeightWTask,
+        //                             resizable:true,
+        //                             accessor: 'WeightWTask'
+        //                         },
+        //                         {
+        //                             Header: strings.ScaledGradeTask,
+        //                             resizable:true,
+        //                             accessor: 'ScaledGrade'
+        //                         }
+        //                     ]}
+        //                     noDataText={strings.TaskGradeNoData}
+        //                 /></div>);
+        //                 })}
+        //             </div>
+        //         </div>
+        //     );
+        // }
 
+        // else return (
+        //     <div className="section card-2 sectionTable">
+        //         <h2 className="title">{strings.TGFRHeader + ": " + taskID}</h2>
+        //         <div className="section-content">
+        //             <div className="col-xs-6">
+        //             {TableTGFRGradeDataFrame.map((taskGradeData, index) => {
+        //                     return (
+        //                     <div key={index}>
+        //                     <div><b><h3>{singleTaskGrade ? "" : "Grading Task #" + (index + 1)}</h3></b></div>  <TableComponent
+        //                     data={taskGradeData}
+        //                     columns={[
+        //                         {
+        //                             Header: strings.Field,
+        //                             accessor: 'Field',
+        //                             resizable:true      
+        //                         },
+        //                         {
+        //                             Header: strings.Type,
+        //                             accessor: 'Type',
+        //                             resizable:true
+        //                         },
+        //                         {         
+        //                             Header: strings.Value,
+        //                             accessor: 'Value',
+        //                             resizable:true                              
+        //                         },
+        //                         {
+        //                             Header: strings.Max,
+        //                             resizable:true,
+        //                             accessor: 'Max'
+        //                         },
+        //                         {
+        //                             Header: strings.ConvertedNumericValue,
+        //                             accessor: 'ConvertedNumericValue',
+        //                             resizable: true
+        //                         },
+        //                         {
+        //                             Header: strings.WeightWTask,
+        //                             resizable:true,
+        //                             accessor: 'WeightWTask'
+        //                         },
+        //                         {
+        //                             Header: strings.ScaledGradeTask,
+        //                             resizable:true,
+        //                             accessor: 'ScaledGrade'
+        //                         }
+        //                     ]}
+        //                     noDataText={strings.TaskGradeNoData}
+        //                 /></div>);
+        //                 })}
+        //             </div>
+        //         </div>
+        //     </div>
+        // );
 
         return (
-            <div className="section card-2 sectionTable">
-                <h2 className="title">{strings.TGFRHeader}</h2>
-                <div className="section-content">
-                    <div className="col-xs-6">
-                        <TableComponent
-                            data={TableTGFRGradeData}
-                            columns={[
-                                {
-                                    Header: strings.Field,
-                                    accessor: 'Field',
-                                    resizable:true      
-                                },
-                                {
-                                    Header: strings.Type,
-                                    accessor: 'Type',
-                                    resizable:true
-                                },
-                                {         
-                                    Header: strings.Value,
-                                    accessor: 'Value',
-                                    resizable:true                              
-                                },
-                                {
-                                    Header: strings.Max,
-                                    resizable:true,
-                                    accessor: 'Max'
-                                },
-                                {
-                                    Header: strings.ConvertedNumericValue,
-                                    accessor: 'ConvertedNumericValue',
-                                    resizable: true
-                                },
-                                {
-                                    Header: strings.WeightWTask,
-                                    resizable:true,
-                                    accessor: 'WeightWTask'
-                                },
-                                {
-                                    Header: strings.ScaledGradeTask,
-                                    resizable:true,
-                                    accessor: 'ScaledGrade'
-                                }
-                            ]}
-                            noDataText={strings.TaskGradeNoData}
-                        />
-                    </div>
-                </div>
-            </div>
+            <MultiTaskGradeTable
+                multiTaskGradeFieldsData={TableTGFRGradeDataFrame}
+                singleTaskGrade={singleTaskGrade}
+                numOfTaskGrades={numOfTaskGrades}
+                extraCrediTaskGrade={false}
+                taskID={taskID}
+                strings={strings}>
+            </MultiTaskGradeTable>
         );
     
     }
